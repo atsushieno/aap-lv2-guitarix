3be6831af56a9bb23951d2adfba7f82ec65de045
Atsushi Eno <atsushieno@gmail.com>
2023-04-19 15:21:55 +0900

    bump aap-lv2 to get ready for 0.2.6 release.

--- app/src/androidTest/java/org/androidaudioplugin/aap_guitarix/PluginTest.kt
+++ app/src/androidTest/java/org/androidaudioplugin/aap_guitarix/PluginTest.kt
@@ -1,11 +1,12 @@
-package org.androidaudioplugin.aap_mda_lv2
+package org.androidaudioplugin.aap_guitarix
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ServiceTestRule
-import org.androidaudioplugin.AudioPluginHostHelper
+import org.androidaudioplugin.hosting.AudioPluginHostHelper
 import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginServiceTesting
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,8 +17,20 @@ class PluginTest {
     val serviceRule = ServiceTestRule()
 
     @Test
-    fun getPluginServiceInfo() {
-        testing.getPluginServiceInfo()
+    fun queryAudioPluginServices() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val services = AudioPluginHostHelper.queryAudioPluginServices(appContext)
+        assert(services.all { s -> s.plugins.size > 0 })
+        assert(services.any { s -> s.packageName == "org.androidaudioplugin.ports.lv2.guitarix" && s.label == "AAPBareBoneSamplePlugin" })
+    }
+
+    @Test
+    fun testPluginInfo() {
+        testing.testSinglePluginInformation {
+            Assert.assertEquals("lv2:http://guitarix.sourceforge.net/plugins/gx_aclipper_#_aclipper_", it.pluginId)
+            Assert.assertEquals(4, it.parameters.size)
+            Assert.assertEquals(4, it.ports.size)
+        }
     }
 
     @Test
